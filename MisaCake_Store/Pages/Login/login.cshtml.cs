a8797646f30ad5f86f0fcb59e937d1d72aa87e26
trangtrang21 <buitrang21012k1@gmail.com>
2023-03-02 16:12:51 +0700

    Login form

--- MisaCake_Store/Pages/Login/login.cshtml.cs
+++ MisaCake_Store/Pages/Login/login.cshtml.cs
@@ -1,23 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MisaCake_Store.Model;
-using MisaCake_Store.Service.Login;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace MisaCake_Store.Pages.Shared
 {
     public class loginModel : PageModel
     {
+        [BindProperty] public string email { get; set; }
+        [BindProperty] public string password { get; set; }
+
+        [BindProperty(SupportsGet = true)]
         public string err { get; set; }
-        public UserAccount userAcc { get; set; }
         public void OnGet() //chạy khi nó vào login
         {
         }
 
-        public void OnPostLogin(UserAccount user) //chạy khi mình gọi đến nó
+        public ActionResult OnPostLogin() //chạy khi mình gọi đến nó
         {
-            loginService l = new loginService();
-            userAcc = l.login(user).Result;
-            err = "Error";
+            //TEST
+            //return Redirect("Index");
+            //
+            Cake_SellerContext dao = new Cake_SellerContext();
+
+            if (HttpContext.Session.GetString("user") != null)
+            {
+                return RedirectToPage("Dashboard", "Home");
+            }
+
+            var f_password = GetMD5(password);
+            var curUser = dao.UserAccounts.FirstOrDefault(acc => acc.Email.Equals(email) && acc.Password.Equals(f_password));
+            var _err = "";
+            if (curUser != null)
+            {
+                HttpContext.Session.SetString("user", JsonConvert.SerializeObject(curUser));
+                return RedirectToAction("", "Index");
+            }
+            else
+            {
+                _err = "Cannot login! Email or Password does not correct!";
+            }
+
+            return RedirectToPage($"login", new { err = _err });
         }
+
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
     }
+
 }
--- MisaCake_Store/Program.cs
+++ MisaCake_Store/Program.cs
@@ -1,8 +1,22 @@
+ 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().WithRazorPagesRoot("/Pages");
+//.AddRazorPagesOptions(option =>
+//{
+//    option.Conventions.AddPageRoute("/home", "");
+//});
 
+builder.Services.AddDistributedMemoryCache();
+
+//add session for 1800s = 10mins
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(1800);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,10 +26,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapRazorPages();
+//    endpoints.MapControllerRoute(
+//        name: "default",
+//        pattern: "{controller=login}/{action=OnPostfLogin}");
+//});
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
